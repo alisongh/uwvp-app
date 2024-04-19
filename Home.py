25be2c903cca1f9d90e6766b8b5edc884fb573fa
alisonggg <liwenh@umich.edu>
2024-04-18 22:44:24 -0400

    code hygiene

--- Home.py
+++ Home.py
@@ -5,7 +5,8 @@ from string import Template
 
 config_data = backend.load_data(r"config.json")
 st.set_page_config(page_title="Survey Data Analysis Application")
-st.markdown("# <div style='text-align: center;'>Survey Data Analysis Application</div>", unsafe_allow_html=True)
+st.markdown("# <div style='text-align: center;'>Survey Data Analysis Application</div>", 
+            unsafe_allow_html=True)
 st.divider()
 with open("Home.md", "r") as file:
     st.markdown(file.read())
